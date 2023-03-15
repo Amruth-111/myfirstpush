28a2dbf2797781d73fe17d51c9d0cf6921042b0b
Amruth-111 <amruthkrishnaa@gmail.com>
2023-03-16 01:22:02 +0530

    get element by tag name

--- doms.js
+++ doms.js
@@ -4,5 +4,12 @@ let additems=document.getElementsByClassName("list-group-item");
 additems[2].style.backgroundColor="green";
 for(let i=0;i<additems.length;i++){
     additems[i].style.fontWeight="bold";
+    additems[i].style.color="red";//boom boom didnt turn into red.
 }
 
+
+// let li=document.getElementsByTagName("li");
+// for(let i=0;i<li.length;i++){
+//     li[i].style.fontWeight="bold";
+//     li[i].style.color="red";//all li tags turns red including boom boom;
+// }
